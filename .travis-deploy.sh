0d2928764a1099a1f6033728faf75e1967960cce
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2016-12-08 13:15:49 +0900

    Travis で JavaDoc 生成時に使う言語の設定を削除

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -e
 
-
-export LANGUAGE=ja_JP.UTF-8
-export LANG=ja_JP.UTF-8
-export LC_ALL=ja_JP.UTF-8
-
-
 deploy_doc() {
   ./gradlew :${1}:javadoc
   tar cvzf ${1}.tar.gz -C ${1}/build/docs/ javadoc
